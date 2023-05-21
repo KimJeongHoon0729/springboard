6ee7e3eff8a1221d92c4bc81fcd334acdeb88a3c
orinaldaramg <duck729039@gmail.com>
2023-05-21 20:12:49 +0900

    휴대전화번호 검증 추가

---
+++ src/main/java/org/orinaldaramg/cotrollers/members/MemberController.java
@@ -0,0 +1,33 @@
+package org.orinaldaramg.cotrollers.members;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.mapping.Join;
+import org.orinaldaramg.models.member.MemberSaveService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+    private MemberSaveService saveService;
+
+    @GetMapping("/join")
+    public String join(){
+        return "member/join";
+    }
+
+    @PostMapping("/join")
+    public String joinPs(@Valid JoinForm joinForm, Errors errors){
+        if(errors.hasErrors()){
+            return "member/join";
+        }
+        saveService.save(joinForm);
+
+        return "redirect:/member/login";
+    }
+}
